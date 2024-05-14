bbf73775dea5d41ba0dd43123e6de1f8d02b6a46
digitAI-4N6 <135169853+digitAI-4N6@users.noreply.github.com>
2024-05-14 17:06:37 -0400

    add classNames to sections
    add classNames to sections

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,9 @@
-import restaurant from "../images/restaurant.jpg";
 import ReservationDetails from "./ReservationDetails";
 import BillingInformation from "./BillingInformation";
 
 export default function Main() {
     return (
         <>
-        <article>
-        <img src={restaurant} alt="restaurant"></img>
-        </article>
         <ReservationDetails />
         <BillingInformation />
         </>
