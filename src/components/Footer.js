de4edd13d5c1a2a18c6b55a3b140ecd08e2b99c9
digitAI-4N6 <135169853+digitAI-4N6@users.noreply.github.com>
2024-05-14 17:16:04 -0400

    fix classNames and unordered lists
    fix classNames and unordered lists

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,45 +3,47 @@ import Logo from "../images/Logo .svg";
 export default function Footer() {
   return (
     <>
-    <article className="foot">
-      <img src={Logo} alt="Logo"/>
-      <h3 className="list">Doormat Navigation</h3>
-      <ul>
-        <li>
-          <a href="">Home</a>
-        </li>
-        <li>
-          <a href="">About</a>
-        </li>
-        <li>
-          <a href="">Menu</a>
-        </li>
-        <li>
-          <a href="">Reservations</a>
-        </li>
-        <li>
-          <a href="">Order Online</a>
-        </li>
-        <li>
-          <a href="">Login</a>
-        </li>
-      </ul>
-      <h3 className="list">Contact</h3>
-      <ul>
-        <li>Address</li>
-        <li>Phone Number</li>
-        <li>Email</li>
-      </ul>
-      <h3 className="list">Social Media Links</h3>
-      <li>
-        <a href="">Fakebook</a>
-      </li>
-      <li>
-        <a href="">Infogram</a>
-      </li>
-      <li>
-        <a href="">Punterest</a>
-      </li>
+      <article className="foot">
+        <img src={Logo} alt="Logo" />
+        <h3>Doormat Navigation</h3>
+        <ul className="list">
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Menu</a>
+          </li>
+          <li>
+            <a href="">Reservations</a>
+          </li>
+          <li>
+            <a href="">Order Online</a>
+          </li>
+          <li>
+            <a href="">Login</a>
+          </li>
+        </ul>
+        <h3>Contact</h3>
+        <ul className="list">
+          <li>Address</li>
+          <li>Phone Number</li>
+          <li>Email</li>
+        </ul>
+        <h3>Social Media Links</h3>
+        <ul>
+          <li>
+            <a href="">Fakebook</a>
+          </li>
+          <li>
+            <a href="">Infogram</a>
+          </li>
+          <li>
+            <a href="">Punterest</a>
+          </li>
+        </ul>
       </article>
     </>
   );
