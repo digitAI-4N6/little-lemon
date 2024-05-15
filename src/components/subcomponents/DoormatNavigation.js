44bce564f84f5d66c36c54855732b87a444428fa
digitAI-4N6 <135169853+digitAI-4N6@users.noreply.github.com>
2024-05-15 12:25:27 -0400

    Create Doormat component
    Create Doormat component

---
+++ src/components/subcomponents/DoormatNavigation.js
@@ -0,0 +1,27 @@
+export default function DoormatNavigation() {
+    return (
+        <>
+        <h3>Doormat Navigation</h3>
+        <ul>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Menu</a>
+          </li>
+          <li>
+            <a href="">Reservations</a>
+          </li>
+          <li>
+            <a href="">Order Online</a>
+          </li>
+          <li>
+            <a href="">Login</a>
+          </li>
+        </ul>
+        </>
+    )
+}
