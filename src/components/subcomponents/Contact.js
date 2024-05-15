08c9b898bc2dba72e66c2f1685e6234e9fae01a1
digitAI-4N6 <135169853+digitAI-4N6@users.noreply.github.com>
2024-05-15 12:36:03 -0400

    make contact an article
    make contact an article

--- src/components/subcomponents/Contact.js
+++ src/components/subcomponents/Contact.js
@@ -1,12 +1,14 @@
 export default function Contact() {
     return (
         <>
+        <article className="footList">
         <h3>Contact</h3>
         <ul>
           <li>Address</li>
           <li>Phone Number</li>
           <li>Email</li>
         </ul>
+        </article>
         </>
     )
 }
