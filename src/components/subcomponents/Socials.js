6242e1d71b90d2aa819b1117525df6d634ba5c42
digitAI-4N6 <135169853+digitAI-4N6@users.noreply.github.com>
2024-05-15 12:25:14 -0400

    Create Socials component
    Create Socials component

---
+++ src/components/subcomponents/Socials.js
@@ -0,0 +1,18 @@
+export default function Socials() {
+    return (
+        <>
+        <h3>Social Media Links</h3>
+        <ul>
+          <li>
+            <a href="">Fakebook</a>
+          </li>
+          <li>
+            <a href="">Infogram</a>
+          </li>
+          <li>
+            <a href="">Punterest</a>
+          </li>
+        </ul>
+        </>
+    )
+}
