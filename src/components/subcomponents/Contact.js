abc63fb4b9461f9818328a5fab65608a83c4dfcf
digitAI-4N6 <135169853+digitAI-4N6@users.noreply.github.com>
2024-05-15 12:25:39 -0400

    Create Contact component
    Create Contact component

---
+++ src/components/subcomponents/Contact.js
@@ -0,0 +1,12 @@
+export default function Contact() {
+    return (
+        <>
+        <h3>Contact</h3>
+        <ul>
+          <li>Address</li>
+          <li>Phone Number</li>
+          <li>Email</li>
+        </ul>
+        </>
+    )
+}
