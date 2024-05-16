ad12b7ebdae8408de4fa0f88ba4a50d348606d98
digitAI-4N6 <135169853+digitAI-4N6@users.noreply.github.com>
2024-05-16 16:36:45 -0400

    Enter content
    Enter content

--- src/components/ReservationDetails.js
+++ src/components/ReservationDetails.js
@@ -1,5 +1,13 @@
 export default function ReservationDetails() {
     return (
-        <p>Reservation Details</p>
+        <>
+        <h2>Reservation Details</h2>
+        <label>Number of Guests</label>
+        <input type="number" min="0" max="10"/>
+        <label>Date</label>
+        <input type="date" value="2024-05-16" min="2024-05-16" max="2024-12-16"/>
+        <label>Time</label>
+        <input type="time" min="9:00" max="21:00"/>
+        </>
     )
 }
