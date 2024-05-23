c91bb81b0c759d99826b7c4ae3df8246e25a0cb3
digitAI-4N6 <135169853+digitAI-4N6@users.noreply.github.com>
2024-05-23 10:22:19 -0400

    add form tag
    add form tag

--- src/components/ReservationDetails.js
+++ src/components/ReservationDetails.js
@@ -1,6 +1,7 @@
 export default function ReservationDetails() {
     return (
         <>
+        <form className="reservation">
         <h2>Reservation Details</h2>
         <label>Number of Guests</label>
         <input type="number" min="0" max="10"/>
@@ -8,6 +9,7 @@ export default function ReservationDetails() {
         <input type="date" value="2024-05-16" min="2024-05-16" max="2024-12-16"/>
         <label>Time</label>
         <input type="time" min="9:00" max="21:00"/>
+        </form>
         </>
     )
 }
