8cee8d274457d01b268884ba914a1e70ced4f771
digitAI-4N6 <135169853+digitAI-4N6@users.noreply.github.com>
2024-05-23 11:39:01 -0400

    connect labels to inputs
    connect labels to inputs

--- src/components/ReservationDetails.js
+++ src/components/ReservationDetails.js
@@ -1,14 +1,14 @@
 export default function ReservationDetails() {
     return (
         <>
-        <form className="reservation">
         <h2>Reservation Details</h2>
-        <label>Number of Guests</label>
-        <input type="number" min="0" max="10"/>
-        <label>Date</label>
-        <input type="date" value="2024-05-16" min="2024-05-16" max="2024-12-16"/>
-        <label>Time</label>
-        <input type="time" min="9:00" max="21:00"/>
+        <form className="reservation">
+        <label for="guests" className="field">Number of Guests</label>
+        <input type="number" min="0" max="10" id="guests" name="guests"/>
+        <label for="date" className="field">Date</label>
+        <input type="date" value="2024-05-16" min="2024-05-16" max="2024-12-16" id="date" name="date"/>
+        <label for="time" className="field">Time</label>
+        <input type="time" min="9:00" max="21:00" id="time" name="time"/>
         </form>
         </>
     )
