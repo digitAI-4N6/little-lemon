67cb5c3691893f8470928afe594770576ade53d3
digitAI-4N6 <135169853+digitAI-4N6@users.noreply.github.com>
2024-05-23 17:17:07 -0400

    Add heroImg
    Add heroImg

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
-// import restaurant from "../images/restaurant.jpg";
+import restaurant from "../images/restaurant.jpg";
 
 export default function Header() {
     return (
         <><h1 className="header">Make a Reservation</h1>
-        {/* <img src={restaurant} alt="restaurant" className="heroImg"></img> */}
+        <img src={restaurant} alt="restaurant" className="heroImg" height="500px" ></img>
         </>
     )
 }
