4f4eedff804b49cb62115f6099aa79e6bdeb037c
digitAI-4N6 <135169853+digitAI-4N6@users.noreply.github.com>
2024-05-24 16:41:34 -0400

    Remove extra fields
    Remove extra fields

--- src/components/BillingInformation.js
+++ src/components/BillingInformation.js
@@ -8,39 +8,11 @@ export default function BillingInformation() {
   const [date, setDate] = useState("");
   const [time, setTime] = useState("");
 
-  // Basic Info
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-
-  // Address
-  const [address, setAddress] = useState("");
-  const [city, setCity] = useState("");
-  const [state, setState] = useState("");
-  const [zipcode, setZipcode] = useState("");
-
-  // Credit Details
-  const [credit, setCredit] = useState("");
-  const [expiration, setExpiration] = useState("");
-  const [cvv, setCVV] = useState("");
-
   const getIsFormValid = () => {
     return (
       guests &&
       date &&
-      time &&
-      firstName &&
-      lastName &&
-      validateEmail(email) &&
-      phone &&
-      address &&
-      city &&
-      state &&
-      zipcode &&
-      credit &&
-      expiration &&
-      cvv
+      time
     );
   };
 
@@ -48,17 +20,6 @@ export default function BillingInformation() {
     setGuests("");
     setDate("");
     setTime("");
-    setFirstName("");
-    setLastName("");
-    setEmail("");
-    setPhone("");
-    setAddress("");
-    setCity("");
-    setState("");
-    setZipcode("");
-    setCredit("");
-    setExpiration("");
-    setCVV("");
   };
 
   const handleSubmit = (e) => {
