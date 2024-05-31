921425c10c9232aedb6298df579ac5ba393146da
digitAI-4N6 <135169853+digitAI-4N6@users.noreply.github.com>
2024-05-31 11:59:59 -0400

    remove useState import
    remove useState import

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Nav from "./components/Nav";
 import Header from "./components/Header";
 import Main from './components/Main';
 import Footer from './components/Footer';
-import React, { useState } from 'react';
+import React from 'react';
 
 
 function App() {
