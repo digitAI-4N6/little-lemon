3318d87607a2857ef2f36d4a829048a97592386b
digitAI-4N6 <135169853+digitAI-4N6@users.noreply.github.com>
2024-05-31 16:00:37 -0400

    init updateTimes
    init updateTimes

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,7 @@ import Bookings from "./Bookings";
 import React, { useState, useReducer } from "react";
 
 const SET_TIME = 'SET_TIME';
+const UPDATE_TIMES = 'UPDATE_TIMES';
 
 const reducer = (state, action) => {
     switch(action.type) {
@@ -13,7 +14,17 @@ const reducer = (state, action) => {
     }
 }
 
+const updateTimes = (date, state.availableTimes) => {
+    // Logic to determine the new availableTimes based on the selected date
+    const newAvailableTimes = '17:00';
+  
+    // Update the availableTimes state variable using the state-changing function
+    dispatch({ type: 'UPDATE_TIMES', payload: newAvailableTimes });
+  }
+
 const initialState = { availableTimes: '17:00' }
+
+
 
 export default function Main() {
   const [guests, setGuests] = useState("");
